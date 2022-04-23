c4a4d7ddbcc7a37e1b2c68eb906a3d9ecdd048b5
W1F1snow <dimitar.hristijan.najdovski@gmail.com>
2022-04-23 23:13:57 +0200

    fixed grid movement and starting implementing dragging n drop feature

--- app/src/main/java/com/aatesting/bugtracker/fragments/Main/GridFragment.java
+++ app/src/main/java/com/aatesting/bugtracker/fragments/Main/GridFragment.java
@@ -1,26 +1,36 @@
 package com.aatesting.bugtracker.fragments.Main;
 
+import android.content.ClipData;
+import android.content.ClipDescription;
 import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.drawable.Drawable;
+import android.os.Build;
 import android.os.Bundle;
 import android.util.DisplayMetrics;
+import android.util.Log;
+import android.view.DragEvent;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.core.content.ContextCompat;
 
 import com.aatesting.bugtracker.R;
 import com.aatesting.bugtracker.activities.Projects.ProjectsMainActivity;
 import com.aatesting.bugtracker.fragments.FragmentSettings;
 import com.aatesting.bugtracker.modifiedClasses.ModifiedFragment;
 
-public class GridFragment extends ModifiedFragment {
+public class GridFragment extends ModifiedFragment implements View.OnTouchListener, View.OnDragListener {
 
 
     /*
@@ -39,9 +49,90 @@ public class GridFragment extends ModifiedFragment {
 
         ViewGroup layout = (ViewGroup) root.findViewById(R.id.container);
         DrawView drawView = new DrawView(root.getContext());
+        drawView.setMinimumWidth(50000);
+        drawView.setMinimumHeight(50000);
         layout.addView(drawView);
 
+        ImageView test = new ImageView(root.getContext());
+        test.setMinimumHeight(500);
+        test.setMinimumWidth(500);
+        test.setBackgroundColor(Color.RED);
+        test.setTag("test");
+        test.setOnTouchListener(this);
+        //test.setText("Testing");
+
+        ImageView test1 = new ImageView(root.getContext());
+        test1.setMinimumWidth(200);
+        test1.setMinimumHeight(200);
+        test1.setBackgroundColor(Color.YELLOW);
+        test1.setTag("test1");
+        test1.setOnDragListener(this);
+        test1.setPadding(150, 150, 0, 0);
+        layout.addView(test);
+        layout.addView(test1);
         return root;
+    }
+
+    @Override
+    public boolean onTouch(View v, MotionEvent event) {
+        View.DragShadowBuilder mShadow = new View.DragShadowBuilder(v);
+        ClipData.Item item = new ClipData.Item(v.getTag().toString());
+        String[] mimeTypes = {ClipDescription.MIMETYPE_TEXT_PLAIN};
+        ClipData data = new ClipData(v.getTag().toString(), mimeTypes, item);
+
+
+        //  switch (v.getId())
+
+        v.startDragAndDrop(data, mShadow, null, 0);
+
+        return false;
+    }
+
+    @Override
+    public boolean onDrag(View v, DragEvent event)
+    {
+        switch (event.getAction()) {
+            case DragEvent.ACTION_DRAG_STARTED:
+
+                ((ImageView) v).setColorFilter(Color.YELLOW);
+
+                v.invalidate();
+                return true;
+
+            case DragEvent.ACTION_DRAG_ENTERED:
+                ((ImageView) v).setColorFilter(ContextCompat.getColor(root.getContext(), R.color.green), android.graphics.PorterDuff.Mode.MULTIPLY);
+                String clipData = event.getClipDescription().getLabel().toString();
+                Log.wtf("clipdata on entered ", clipData.toString());
+                v.invalidate();
+                return true;
+
+            case DragEvent.ACTION_DRAG_LOCATION:
+                return true;
+
+            case DragEvent.ACTION_DRAG_EXITED:
+                ((ImageView) v).clearColorFilter();
+                ((ImageView) v).setColorFilter(Color.YELLOW);
+
+                v.invalidate();
+                return true;
+
+            case DragEvent.ACTION_DROP:
+                clipData = event.getClipDescription().getLabel().toString();
+                Log.wtf("clipdata on success ", clipData.toString());
+
+                Toast.makeText(root.getContext(), "dropped at right coordinates", Toast.LENGTH_SHORT).show();
+
+                v.invalidate();
+                return true;
+
+            case DragEvent.ACTION_DRAG_ENDED:
+                ((ImageView) v).clearColorFilter();
+
+                Toast.makeText(root.getContext(), "try dropping in the blackish cube", Toast.LENGTH_SHORT).show();
+                return true;
+            default:
+                return false;
+        }
     }
 
     class DrawView extends View {
@@ -49,7 +140,7 @@ public class GridFragment extends ModifiedFragment {
 
         public DrawView(Context context) {
             super(context);
-            paint.setColor(Color.parseColor("#808080"));
+            paint.setColor(Color.parseColor("#606060"));
         }
 
         @Override
@@ -58,18 +149,22 @@ public class GridFragment extends ModifiedFragment {
             DisplayMetrics metrics = getContext().getResources().getDisplayMetrics();
             View topbar = root.findViewById(R.id.topBar);
 
-            float yOff = topbar.getHeight();
+            float spacing = 30f;
 
+            //float yOff = topbar.getHeight();
+            float yOff = 0;
             for (int x = 0; x < 30; x++){
                 for (int y = 0; y < 30; y++){
-                    float xVal = x * 40f * metrics.density + 40f;
-                    float yVal = y * 40f * metrics.density + yOff + 40f;
+                    float xVal = x * spacing * metrics.density + spacing;
+                    float yVal = y * spacing * metrics.density + yOff + spacing;
 
-                    canvas.drawCircle(xVal, yVal, 4, paint);
+                    canvas.drawCircle(xVal, yVal, 2.5f, paint);
                 }
             }
         }
     }
+
+    private void Testing(){}
 
     /*
 
