2d946f1a785377491e7489997f2640f4c069e243
W1F1snow <dimitar.hristijan.najdovski@gmail.com>
2022-04-26 22:20:14 +0200

    drag n drop feature working, started working on implementing stuff the different types of views that can be placed on the grid

--- app/src/main/java/com/aatesting/bugtracker/fragments/Main/GridFragment.java
+++ app/src/main/java/com/aatesting/bugtracker/fragments/Main/GridFragment.java
@@ -6,8 +6,6 @@ import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
-import android.graphics.drawable.Drawable;
-import android.os.Build;
 import android.os.Bundle;
 import android.util.DisplayMetrics;
 import android.util.Log;
@@ -16,10 +14,10 @@ import android.view.LayoutInflater;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.ImageView;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.constraintlayout.widget.ConstraintLayout;
@@ -39,38 +37,71 @@ public class GridFragment extends ModifiedFragment implements View.OnTouchListen
      */
 
     private View root;
+    private GridView gridView;
+    private ViewGroup layout;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         root = inflater.inflate(R.layout.fragment_grid, container, false);
+        layout = (ViewGroup) root.findViewById(R.id.container);
 
         ((ProjectsMainActivity)requireActivity()).thisFragment = this;
         ((ProjectsMainActivity)requireActivity()).listeners(root, FragmentSettings.GRID_FRAGMENT_ID, getParentFragmentManager());
 
-        ViewGroup layout = (ViewGroup) root.findViewById(R.id.container);
-        DrawView drawView = new DrawView(root.getContext());
-        drawView.setMinimumWidth(50000);
-        drawView.setMinimumHeight(50000);
-        layout.addView(drawView);
+        addGrid();
 
-        ImageView test = new ImageView(root.getContext());
-        test.setMinimumHeight(500);
-        test.setMinimumWidth(500);
+        ImageButton test = new ImageButton(root.getContext());
+        test.setMinimumHeight(250);
+        test.setMinimumWidth(250);
         test.setBackgroundColor(Color.RED);
-        test.setTag("test");
-        test.setOnTouchListener(this);
-        //test.setText("Testing");
+        test.setTag("ImgBtn1");
 
-        ImageView test1 = new ImageView(root.getContext());
-        test1.setMinimumWidth(200);
-        test1.setMinimumHeight(200);
-        test1.setBackgroundColor(Color.YELLOW);
-        test1.setTag("test1");
-        test1.setOnDragListener(this);
-        test1.setPadding(150, 150, 0, 0);
-        layout.addView(test);
-        layout.addView(test1);
+        TextView textView = new TextView(root.getContext());
+        textView.setText("I hate this");
+        textView.setPadding(0, 300, 0, 0);
+
+        ConstraintLayout testCon = new ConstraintLayout(root.getContext());
+        testCon.setTag("ImgBtn1Layout");
+        testCon.addView(test);
+        testCon.addView(textView);
+        test.setOnTouchListener(this);
+
+        layout.addView(testCon);
         return root;
+    }
+
+    /**
+     * adds the grid to the fragment
+     */
+    private void addGrid(){
+        gridView = new GridView(root.getContext());
+        gridView.setMinimumWidth(50000);
+        gridView.setMinimumHeight(50000);
+        layout.addView(gridView);
+
+        ImageButton gridColliders = new ImageButton(root.getContext());
+        gridColliders.setMinimumWidth(50000);
+        gridColliders.setMinimumHeight(50000);
+        gridColliders.setBackgroundColor(Color.parseColor("#00000000"));
+        gridColliders.setTag("gridColliders");
+        gridColliders.setOnDragListener(this);
+        gridColliders.setPadding(150, 150, 0, 0);
+        layout.addView(gridColliders);
+    }
+
+    /**
+     * sets position of a given view
+     */
+    private void setPos(String tag, float x, float y){
+        DisplayMetrics metrics = getContext().getResources().getDisplayMetrics();
+        View tagView = root.findViewWithTag(tag + "Layout");
+
+        //in short we are adjusting the position so it follows the dots on the screen
+        float newX = Math.round((x - tagView.getWidth()/2f) / (gridView.spacing * metrics.density)) * gridView.spacing * metrics.density;
+        float newY = Math.round((y - tagView.getHeight()/2f) / (gridView.spacing * metrics.density)) * gridView.spacing * metrics.density;
+
+        tagView.setX(newX);
+        tagView.setY(newY);
     }
 
     @Override
@@ -80,11 +111,7 @@ public class GridFragment extends ModifiedFragment implements View.OnTouchListen
         String[] mimeTypes = {ClipDescription.MIMETYPE_TEXT_PLAIN};
         ClipData data = new ClipData(v.getTag().toString(), mimeTypes, item);
 
-
-        //  switch (v.getId())
-
         v.startDragAndDrop(data, mShadow, null, 0);
-
         return false;
     }
 
@@ -100,45 +127,45 @@ public class GridFragment extends ModifiedFragment implements View.OnTouchListen
                 return true;
 
             case DragEvent.ACTION_DRAG_ENTERED:
-                ((ImageView) v).setColorFilter(ContextCompat.getColor(root.getContext(), R.color.green), android.graphics.PorterDuff.Mode.MULTIPLY);
+                ((ImageButton) v).setColorFilter(ContextCompat.getColor(root.getContext(), R.color.green), android.graphics.PorterDuff.Mode.MULTIPLY);
                 String clipData = event.getClipDescription().getLabel().toString();
-                Log.wtf("clipdata on entered ", clipData.toString());
                 v.invalidate();
                 return true;
 
             case DragEvent.ACTION_DRAG_LOCATION:
+                //Log.wtf("dLoc", "event pos x: " + event.getX() + " y: " + event.getY());
+                //Log.wtf("dLoc", "view pos x: " + v.getX() + " y: " + v.getY());
+
                 return true;
 
             case DragEvent.ACTION_DRAG_EXITED:
-                ((ImageView) v).clearColorFilter();
-                ((ImageView) v).setColorFilter(Color.YELLOW);
+                ((ImageButton) v).clearColorFilter();
+                ((ImageButton) v).setColorFilter(Color.YELLOW);
 
                 v.invalidate();
                 return true;
 
             case DragEvent.ACTION_DROP:
                 clipData = event.getClipDescription().getLabel().toString();
-                Log.wtf("clipdata on success ", clipData.toString());
 
-                Toast.makeText(root.getContext(), "dropped at right coordinates", Toast.LENGTH_SHORT).show();
-
+                setPos(clipData, event.getX(), event.getY());
                 v.invalidate();
                 return true;
 
             case DragEvent.ACTION_DRAG_ENDED:
-                ((ImageView) v).clearColorFilter();
-
-                Toast.makeText(root.getContext(), "try dropping in the blackish cube", Toast.LENGTH_SHORT).show();
+                ((ImageButton) v).clearColorFilter();
                 return true;
             default:
                 return false;
         }
     }
 
-    class DrawView extends View {
+    class GridView extends View {
+        public float spacing = 30f;
+
         Paint paint = new Paint();
 
-        public DrawView(Context context) {
+        public GridView(Context context) {
             super(context);
             paint.setColor(Color.parseColor("#606060"));
         }
@@ -155,31 +182,12 @@ public class GridFragment extends ModifiedFragment implements View.OnTouchListen
             float yOff = 0;
             for (int x = 0; x < 30; x++){
                 for (int y = 0; y < 30; y++){
-                    float xVal = x * spacing * metrics.density + spacing;
-                    float yVal = y * spacing * metrics.density + yOff + spacing;
+                    float xVal = x * spacing * metrics.density;
+                    float yVal = y * spacing * metrics.density + yOff;
 
                     canvas.drawCircle(xVal, yVal, 2.5f, paint);
                 }
             }
         }
     }
-
-    private void Testing(){}
-
-    /*
-
-                <ImageView
-        android:id="@+id/createImg"
-        android:layout_width="wrap_content"
-        android:layout_height="wrap_content"
-        android:layout_marginStart="8dp"
-        android:layout_marginTop="12dp"
-        android:layout_marginBottom="8dp"
-        android:background="@drawable/transparent"
-        android:src="@drawable/ic_add_24dp"
-        app:layout_constraintBottom_toBottomOf="parent"
-        app:layout_constraintStart_toStartOf="parent"
-        app:layout_constraintTop_toBottomOf="@+id/weeksRecyclerView" />
-
-     */
 }
