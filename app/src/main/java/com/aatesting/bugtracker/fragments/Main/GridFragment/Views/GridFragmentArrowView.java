e161983aacff43e91fda9762176f16411f9bbfb9
W1F1snow <dimitar.hristijan.najdovski@gmail.com>
2022-04-30 20:16:43 +0200

    figured out how to do rotation

--- app/src/main/java/com/aatesting/bugtracker/fragments/Main/GridFragment/Views/GridFragmentArrowView.java
+++ app/src/main/java/com/aatesting/bugtracker/fragments/Main/GridFragment/Views/GridFragmentArrowView.java
@@ -15,9 +15,7 @@ public class GridFragmentArrowView extends View {
     private float dp;
     private float yOff;
 
-    float xs = (120 + 45) * dp, ys = (450) * dp - yOff;
-    float xe = (210 + 45) * dp, ye = (210) * dp + yOff;
-    float xb = 200 * dp,        yb = 400 * dp;
+
 
     public GridFragmentArrowView(Context context, float xOff, float yOff){
         super(context);
@@ -46,6 +44,10 @@ public class GridFragmentArrowView extends View {
         //gBoardView(120, 450); //bottom one
         //gBoardView(210, 90); //top one
 
+        float xs = (120 + 45) * dp, ys = (450) * dp - yOff;
+        float xe = (210 + 45) * dp, ye = (210) * dp + yOff;
+        float xb = 200 * dp,        yb = 400 * dp;
+
         //define paths
         Path linePath = new Path();
         linePath.moveTo(xs, ys); //starting point
@@ -60,49 +62,40 @@ public class GridFragmentArrowView extends View {
             arrowPath.lineTo(30 * xScal, 120 * yScal);
         }
 
-        double angle = calculateAngle(xs, xe, ys, ye);
+        double angle = calculateAngle(xs, xe, ys, ye) * Math.PI/180;
+        Log.wtf("ERROR", angle + "");
+        Point point = new Point(xs, ys);
+        rotate_point(xe, ye, angle, point);
 
+        canvas.drawCircle(xs, ys, 5 * dp, linePaint);
+        canvas.drawCircle(xe, ye, 5 * dp, linePaint);
 
-        float x2e = 0 * 30 + 5 * 30;
-        float y2e = 0 * 30 + 5 * 30;
-        float x2s = 3 * 30 + 5 * 30;
-        float y2s = 0 * 30 + 5 * 30;
-
-
-        canvas.drawCircle(x2s * dp, y2s *dp, 5 * dp, linePaint);
-        canvas.drawCircle(x2e *dp, y2e * dp, 5 * dp, linePaint);
-
-
-        double sinX = Math.sin(90 * Math.PI /180);
-        double cosX = Math.cos(90 * Math.PI / 180);
-
-
-        Log.wtf("FUCK YOU", x2s + " " + y2s + " " + x2e + " " + y2e);
-        x2s -= (x2s - x2e);
-        y2s -= (y2s - y2e);
-        Log.wtf("FUCK YOU", x2s + " " + y2s);
-
-
-        float xnew = (float) (x2s * sinX - y2s * cosX);
-        float ynew = (float) (x2s * sinX +  y2s * cosX);
-
-        x2s = xnew + (x2e);
-        y2s = ynew + y2e;
-
-        canvas.drawCircle(x2s * dp, y2s * dp, 5 * dp, linePaint);
-
-
-
-
+        canvas.drawCircle(point.x, point.y, 5 * dp, linePaint);
 
         //draw paths
         //canvas.drawPath(linePath, linePaint);
         //canvas.drawPath(arrowPath, arrowPaint);
         //canvas.drawCircle(x3, y3, 10 * dp, arrowPaint);
         //canvas.drawLine(xs, ys, xe, ye, linePaint);
-        //canvas.drawRect(120 * dp, 450 * dp, (120 * dp) + (89.818184f * dp), (450 * dp) + (117.09091f * dp), linePaint);
-        //canvas.drawRect(210 * dp, 90 * dp, (210 * dp) + (89.818184f * dp), (90 * dp) + (117.09091f * dp), linePaint);
+        canvas.drawRect(120 * dp, 450 * dp, (120 * dp) + (89.818184f * dp), (450 * dp) + (117.09091f * dp), linePaint);
+        canvas.drawRect(210 * dp, 90 * dp, (210 * dp) + (89.818184f * dp), (90 * dp) + (117.09091f * dp), linePaint);
 
+    }
+
+    static Point rotate_point(double cx, double cy, double angle, Point p)
+    {
+        double s = Math.sin(angle);
+        double c = Math.cos(angle);
+        // translate point back to origin:
+        p.x -= cx;
+        p.y -= cy;
+        // rotate point
+        double Xnew = p.x * c - p.y * s;
+        double Ynew = p.x * s + p.y * c;
+        // translate point back:
+        p.x = (float) (Xnew + cx);
+        p.y = (float) (Ynew + cy);
+        return p;
     }
 
     /**
@@ -122,9 +115,8 @@ public class GridFragmentArrowView extends View {
     private double calculateAngle(float startX, float endX, float startY, float endY){
         float deltaX = startX - endX;
         float deltaY = startY - endY;
-        double result = Math.toDegrees(Math.atan2(deltaY, deltaX));
 
-        return result;
+        return Math.toDegrees(Math.atan2(deltaY, deltaX));
     }
 
     private Paint linePaint(){
@@ -152,5 +144,19 @@ public class GridFragmentArrowView extends View {
         arrowPaint.setStrokeWidth(5);
 
         return arrowPaint;
+    }
+
+    /**
+     * nothing else wants to work, I will go crazy if I spend another hour trying to figure out why it doesn't work
+     */
+    static class Point
+    {
+        public float x;
+        public float y;
+
+        public Point(float x, float y){
+            this.x = x;
+            this.y = y;
+        }
     }
 }
